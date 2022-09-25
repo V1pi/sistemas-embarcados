f9ea5850f40fd162e347a2e52303bf67c9a26fdc
v1pi <vimivini99@gmail.com>
2022-09-25 19:55:49 -0300

    Adicionado contador em anel

---
+++ sap1-verilog/src/contador_anel.v
@@ -0,0 +1,17 @@
+// contador_anel - contador em anel de 6 estados
+module contador_anel (
+  input n_clk,
+  input n_clr,
+  output reg [5:0] q
+);
+  
+    always @(negedge n_clk or negedge n_clr) begin
+      if (!n_clr)
+        q <= 6'b000001;
+      else begin
+        q <= q<<1;
+        q[0] <= q[5];
+      end
+    end
+
+endmodule
